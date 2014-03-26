82b0b611c813832c55c7b19574d04c067215627d
Ash krishan <ash.krishan@linux.com>
2014-03-26 12:00:51 +0000

    Refactored FT to include helper method

--- functional-test.py
+++ functional-test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+    
+    def check_for_row_in_list(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
         
     def test_can_crewat_new_list_and_retrieve_new_list(self):
         
@@ -30,24 +35,23 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.clear()
         inputbox.send_keys('Buy weekly shopping')
         inputbox.send_keys(Keys.ENTER)
-
+        
+        self.check_for_row_in_list('1: Buy weekly shopping')
+        #self.check_for_row_in_list('2: Buy two pints of Milk')
 #Jane clicks on the new button and weekly shopping is added to a table list
         #time.sleep(10)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text=='1: Buy weekly shopping' for row in rows), 'New item did not appear in list current text is \n%s' % (table.text))
+        #self.assertTrue(any(row.text=='1: Buy weekly shopping' for row in rows), 'New item did not appear in list current text is \n%s' % (table.text))
         
         #Second item added by jane 
         inputbox = self.browser.find_element_by_id('id_new_item')
         #self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 #She types "Buy two pints of Milk" in the list
-        inputbox.clear()
+        #inputbox.clear()
         inputbox.send_keys('Buy two pints of Milk')
         inputbox.send_keys(Keys.ENTER)
         
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: Buy two pints of Milk', [row.text for row in rows])
+        self.check_for_row_in_list('1: Buy weekly shopping')
+        self.check_for_row_in_list('2: Buy two pints of Milk')
         
         #She adds another item to the t-do list
         self.fail('Finish the test!')
