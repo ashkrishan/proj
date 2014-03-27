734c6aba905dc7baf951c18f7272c8f471e598ba
Ash krishan <ash.krishan@linux.com>
2014-03-27 13:02:22 +0000

    new user added to FT to test new url for new user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,6 +5,7 @@ import unittest
 #import time
 import os
 
+##This is required for live testing server to point to correct port otherwise OS error is thrown by win 7
 os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = 'localhost:8082'
 class NewVisitorTest(LiveServerTestCase):
     
@@ -39,7 +40,9 @@ class NewVisitorTest(LiveServerTestCase):
         #inputbox.clear()
         inputbox.send_keys('Buy weekly shopping')
         inputbox.send_keys(Keys.RETURN)
-        
+        #Jane is taken to her own url for her lists
+        jane_list_url = self.browser.current_url
+        self.assertRegex(jane_list_url,'/lists/.+')        
         self.check_for_row_in_list('1: Buy weekly shopping')
         #self.check_for_row_in_list('2: Buy two pints of Milk')
 #Jane clicks on the new button and weekly shopping is added to a table list
@@ -54,8 +57,20 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy two pints of Milk')
         inputbox.send_keys(Keys.RETURN)
         
+        #Page updates again and Jane sees below 2 items in her own list
         self.check_for_row_in_list('1: Buy weekly shopping')
         self.check_for_row_in_list('2: Buy two pints of Milk')
+        
+        #New user Francis comes along and presented with new url        
+        ##We use a new browser session to make sure no session, cokkies etc are being used from previous user
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        
+        #New url is presented ot Francis and he can't see any sign of Jane
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy weekly shopping',page_text)
+        self.assertNotIn('Buy two pints of Milk',page_text)
         
         #She adds another item to the t-do list
         self.fail('Finish the test!')
